4680cf17215a8c946db57351b0c2e2077f0cdec1
Alejandro Henao <johan.alejo.1099@gmail.com>
2018-04-26 15:58:07 -0500

    Recolected data

--- Grab Objects 2 Hands/Assets/Scripts/RotarObjeto.cs
+++ Grab Objects 2 Hands/Assets/Scripts/RotarObjeto.cs
@@ -19,6 +19,12 @@ public class RotarObjeto : MonoBehaviour
     //Cuando se esta rotando e interpolando el angulo, si por ejemplo, el angulo que (-45, 0, 45), entonces queda una rotacion que hace voltear al objeto, alrededor de -16 en el eje Z
     //y si se suma con el valor que deberia estar, quedaria (-45, -29, 45), y -29 - 16 = -45, por lo que hay relacion en esa rotacion tambien
 
+    //El eje Y con el Vector -29.99999, 0, 60,00001 necesita -22.37 en el valor Y del vector para acomodarse a su rotacion
+    //El eje Y con el Vector -60.00001, 0, 29,99999 necesita -33.435 en el valor Y del vector para acomodarse a su rotacion
+
+    //-22.37  -10.472
+
+    //Si la z esta entre 1 y 179, se puede girar alrededor de este sin ningun problema
 
     public DireccionUp direccionUp;
 
@@ -26,9 +32,11 @@ public class RotarObjeto : MonoBehaviour
 
     public Vector3 ejeRotacion;
 
-    public float anguloHorizontal;
-    public float anguloVertical;
-    public bool actualizarAngulo;
+    public float valorY;
+
+    public float anguloEjeY;
+    public float anguloEjeZ;
+    public float anguloEjeX;
 
     private void Update()
     {
@@ -47,53 +55,160 @@ public class RotarObjeto : MonoBehaviour
 
     private void ActualizarRotacion()
     {
-        ActualizarEjeRotacion();
-        //verificarProductroPunto();
-        // Vector3 direccion = (derecha.position - izquierda.position).normalized;
-        // if (actualizarAngulo)
-        // {
-        //     Vector3 arriba = Quaternion.Euler(ejeRotacion) * direccion;
-        //     transform.rotation = Quaternion.LookRotation(direccion, arriba);
-        // }
-        // else
-        //     transform.rotation = Quaternion.LookRotation(direccion);
+        VerificarAnguloCartesianoEjeZ();
+        VerificarAnguloCartesianoEjeX();
+        VerificarUp();
+
+        if (direccionUp == DireccionUp.Arriba)
+            VerificarAnguloCartesianoEjeY();
+        else if (direccionUp == DireccionUp.Abajo)
+            VerificarAnguloCartesianoEjeYInverso();
+
+        Vector3 direccion = (derecha.position - izquierda.position).normalized;
+        Vector3 arriba = Quaternion.Euler(ejeRotacion) * direccion;
+        transform.rotation = Quaternion.LookRotation(direccion, arriba);
     }
 
-    // private void verificarProductroPunto()
-    // {
-    //     Vector3 direccion = (derecha.position - izquierda.position).normalized;
-    //     anguloVertical = Vector3.Dot(direccion, Vector3.up);
-    //     direccion.y = 0;
-    //     anguloHorizontal = Vector3.SignedAngle(direccion, Vector3.forward, Vector3.up);
-
-    //     if (anguloHorizontal < -85f && anguloHorizontal > -95f)
-    //     {
-    //         ejeRotacion = new Vector3(0, 0, 90f);
-    //         actualizarAngulo = true;
-    //     }
-    //     else if (anguloHorizontal < 5f && anguloHorizontal > -5f)
-    //     {
-    //         ejeRotacion = new Vector3(-90f, 0, 0);
-    //         actualizarAngulo = true;
-    //     }
-    //     else if (anguloVertical < 0.4f && anguloVertical > -0.4f)
-    //     {
-    //         actualizarAngulo = false;
-    //     }
-    //     else
-    //     {
-    //         actualizarAngulo = true;
-    //     }
-    // }
-
-    private void ActualizarEjeRotacion()
+    private void VerificarAnguloCartesianoEjeY()
     {
         Vector3 direccion = (derecha.position - izquierda.position).normalized;
         direccion.y = 0;
-        anguloHorizontal = Vector3.SignedAngle(direccion, Vector3.forward, Vector3.up);
+        anguloEjeY = Vector3.SignedAngle(direccion, Vector3.forward, Vector3.up);
 
+        if (anguloEjeY <= 0f && anguloEjeY >= -90f)
+            ActualizarEjeCuadranteI(anguloEjeY);
+        else if (anguloEjeY >= 0f && anguloEjeY <= 90f)
+            ActualizarEjeCuadranteII(anguloEjeY);
+        else if (anguloEjeY >= 90f && anguloEjeY <= 180f)
+            ActualizarEjeCuadranteIII(anguloEjeY);
+        else if (anguloEjeY <= -90f && anguloEjeY >= -180f)
+            ActualizarEjeCuadranteIV(anguloEjeY);
+    }
 
+    private void ActualizarEjeCuadranteI(float angulo)
+    {
+        float interpolacion = Mathf.Abs(angulo) / 90f;
+        Vector3 vectorEjeX = new Vector3(-90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, 90f);
 
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeX, vectorEjeZ, interpolacion);
 
+        ejeRotacion = vectorFinal;
+    }
+
+    private void ActualizarEjeCuadranteII(float angulo)
+    {
+        float interpolacion = Mathf.Abs(angulo) / 90f;
+        Vector3 vectorEjeX = new Vector3(-90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, -90f);
+
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeX, vectorEjeZ, interpolacion);
+
+        ejeRotacion = vectorFinal;
+    }
+
+    private void ActualizarEjeCuadranteIII(float angulo)
+    {
+        float interpolacion = (Mathf.Abs(angulo) - 90f) / 90f;
+        Vector3 vectorEjeX = new Vector3(90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, -90f);
+
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeZ, vectorEjeX, interpolacion);
+
+        ejeRotacion = vectorFinal;
+    }
+
+    private void ActualizarEjeCuadranteIV(float angulo)
+    {
+        float interpolacion = (Mathf.Abs(angulo) - 90f) / 90f;
+        Vector3 vectorEjeX = new Vector3(90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, 90f);
+
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeZ, vectorEjeX, interpolacion);
+
+        ejeRotacion = vectorFinal;
+    }
+
+    private void VerificarAnguloCartesianoEjeZ()
+    {
+        Vector3 direccion = (derecha.position - izquierda.position).normalized;
+        direccion.z = 0;
+        anguloEjeZ = Vector3.SignedAngle(direccion, Vector3.up, -Vector3.forward);
+    }
+
+    private void VerificarAnguloCartesianoEjeX()
+    {
+        Vector3 direccion = (derecha.position - izquierda.position).normalized;
+        direccion.x = 0;
+        anguloEjeX = Vector3.SignedAngle(direccion, Vector3.up, Vector3.right);
+    }
+
+    private void VerificarUp()
+    {
+        // if (anguloEjeZ <= 0 && anguloEjeZ >= -180f)
+        //     direccionUp = DireccionUp.Arriba;
+        // else if (anguloEjeZ >= 0 && anguloEjeZ <= 180f)
+        //     direccionUp = DireccionUp.Abajo;
+    }
+
+    private void VerificarAnguloCartesianoEjeYInverso()
+    {
+        Vector3 direccion = (derecha.position - izquierda.position).normalized;
+        // anguloVertical = Vector3.Dot(direccion, Vector3.up);
+        direccion.y = 0;
+        anguloEjeY = Vector3.SignedAngle(direccion, Vector3.forward, Vector3.up);
+
+        if (anguloEjeY <= 0f && anguloEjeY >= -90f)
+            ActualizarEjeCuadranteIDown(anguloEjeY);
+        else if (anguloEjeY >= 0f && anguloEjeY <= 90f)
+            ActualizarEjeCuadranteIIDown(anguloEjeY);
+        else if (anguloEjeY >= 90f && anguloEjeY <= 180f)
+            ActualizarEjeCuadranteIIIDown(anguloEjeY);
+        else if (anguloEjeY <= -90f && anguloEjeY >= -180f)
+            ActualizarEjeCuadranteIVDown(anguloEjeY);
+    }
+
+    private void ActualizarEjeCuadranteIDown(float angulo)
+    {
+        float interpolacion = Mathf.Abs(angulo) / 90f;
+        Vector3 vectorEjeX = new Vector3(90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, -90f);
+
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeX, vectorEjeZ, interpolacion);
+
+        ejeRotacion = vectorFinal;
+    }
+
+    private void ActualizarEjeCuadranteIIDown(float angulo)
+    {
+        float interpolacion = Mathf.Abs(angulo) / 90f;
+        Vector3 vectorEjeX = new Vector3(90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, 90f);
+
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeX, vectorEjeZ, interpolacion);
+
+        ejeRotacion = vectorFinal;
+    }
+
+    private void ActualizarEjeCuadranteIIIDown(float angulo)
+    {
+        float interpolacion = (Mathf.Abs(angulo) - 90f) / 90f;
+        Vector3 vectorEjeX = new Vector3(-90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, 90f);
+
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeZ, vectorEjeX, interpolacion);
+
+        ejeRotacion = vectorFinal;
+    }
+
+    private void ActualizarEjeCuadranteIVDown(float angulo)
+    {
+        float interpolacion = (Mathf.Abs(angulo) - 90f) / 90f;
+        Vector3 vectorEjeX = new Vector3(-90f, valorY, 0f);
+        Vector3 vectorEjeZ = new Vector3(0f, valorY, -90f);
+
+        Vector3 vectorFinal = Vector3.Lerp(vectorEjeZ, vectorEjeX, interpolacion);
+
+        ejeRotacion = vectorFinal;
     }
 }
--- Grab Objects 2 Hands/Assets/Scripts/RotarSimple.cs
+++ Grab Objects 2 Hands/Assets/Scripts/RotarSimple.cs
@@ -10,6 +10,7 @@ public class RotarSimple : MonoBehaviour
     {
         ActualizarPosicion();
         ActualizarRotacion();
+        //ZLockedAim();
 
         Debug.DrawRay(transform.position, transform.forward, Color.blue);
         Debug.DrawRay(transform.position, transform.right, Color.red);
@@ -26,5 +27,57 @@ public class RotarSimple : MonoBehaviour
         Vector3 direccion = (derecha.position - izquierda.position).normalized;
         Vector3 arriba = Quaternion.Euler(ejeRotacion) * direccion;
         transform.rotation = Quaternion.LookRotation(direccion, arriba);
+
+        //transform.rotation = Quaternion.LookRotation(izquierda.transform.position - derecha.transform.position, izquierda.transform.TransformDirection(Vector3.forward));
+    }
+
+    protected virtual void ZLockedAim()
+    {
+        Vector3 forward = (izquierda.transform.position - derecha.transform.position).normalized;
+
+        // calculate rightLocked rotation
+        Quaternion rightLocked = Quaternion.LookRotation(forward, Vector3.Cross(-izquierda.transform.right, forward).normalized);
+
+        // delta from current rotation to the rightLocked rotation
+        Quaternion rightLockedDelta = Quaternion.Inverse(transform.rotation) * rightLocked;
+
+        float rightLockedAngle;
+        Vector3 rightLockedAxis;
+
+        // forward direction and roll
+        rightLockedDelta.ToAngleAxis(out rightLockedAngle, out rightLockedAxis);
+
+        if (rightLockedAngle > 180f)
+        {
+            // remap ranges from 0-360 to -180 to 180
+            rightLockedAngle -= 360f;
+        }
+
+        // make any negative values into positive values;
+        rightLockedAngle = Mathf.Abs(rightLockedAngle);
+
+        // calculate upLocked rotation
+        Quaternion upLocked = Quaternion.LookRotation(forward, derecha.transform.forward);
+
+        // delta from current rotation to the upLocked rotation
+        Quaternion upLockedDelta = Quaternion.Inverse(transform.rotation) * upLocked;
+
+        float upLockedAngle;
+        Vector3 upLockedAxis;
+
+        // forward direction and roll
+        upLockedDelta.ToAngleAxis(out upLockedAngle, out upLockedAxis);
+
+        // remap ranges from 0-360 to -180 to 180
+        if (upLockedAngle > 180f)
+        {
+            upLockedAngle -= 360f;
+        }
+
+        // make any negative values into positive values;
+        upLockedAngle = Mathf.Abs(upLockedAngle);
+
+        // assign the one that involves less change to roll
+        transform.rotation = (upLockedAngle < rightLockedAngle ? upLocked : rightLocked);
     }
 }
